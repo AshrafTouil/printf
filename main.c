bbf54189f3ca8a6f43fac5e3f659025010424f53
dinaalilech <larosa.asistencia@gmail.com>
2023-07-25 07:10:49 -0700

    update

--- main.c
+++ main.c
@@ -9,46 +9,10 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
-    int len;
-    int len2;
-    unsigned int ui;
-    void *addr;
-
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
-    ui = (unsigned int)INT_MAX + 1024;
-    addr = (void *)0x7ffe637541f0;
-    _printf("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len2, len2);
-    _printf("Negative:[%d]\n", -762534);
-    printf("Negative:[%d]\n", -762534);
-    _printf("Unsigned:[%u]\n", ui);
-    printf("Unsigned:[%u]\n", ui);
-    _printf("Unsigned octal:[%o]\n", ui);
-    printf("Unsigned octal:[%o]\n", ui);
-    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    _printf("Character:[%c]\n", 'H');
-    printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
-    _printf("Address:[%p]\n", addr);
-    printf("Address:[%p]\n", addr);
-    len = _printf("Percent:[%%]\n");
-    len2 = printf("Percent:[%%]\n");
-    _printf("Len:[%d]\n", len);
-    printf("Len:[%d]\n", len2);
-    _printf("Unknown:[%r]\n");
-    printf("Unknown:[%r]\n");
-    return (0);
-=======
-	int n;
 	int len;
 	int len2;
 	unsigned int ui;
 	void *addr;
-
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
 	ui = (unsigned int)INT_MAX + 1024;
@@ -74,20 +38,6 @@ int main(void)
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
 	_printf("Unknown:[%r]\n");
-	n = _printf("DZEKO YOHIBO STRANGER\n");
-	printf("%d\n", n);
-	len = _printf("[%s]\n", "hi hi hi");
-	len2 = printf("[%s]\n", "hi hi hi");
-	len = printf("Len:[%d]\n", len);
-	len2 = printf("Len:[%d]\n", len2);
-	len = _printf("wach khdmat: %c. gul lya?\n", 48);
-	_printf("%-1\n");
-	_printf("%i\n", 567);
-	_printf("%d\n", 345);
-	_printf("%b\n", 98);
-	_printf("%b\n", 2653623);
-	_printf("%b\n", 1);
-	_printf("%b\n", 0);
+	printf("Unknown:[%r]\n");
 	return (0);
->>>>>>> 8983d6929dd9f5fd39a44daa3ad6ea09b8e2ebca
 }
